5a89bff861906f7b6c159c1a08be898bf582b4fe
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-31 16:53:16 +0100

    migrate new table on database

--- database/migrations/2022_12_30_162549_create_letters_table.php
+++ database/migrations/2022_12_30_162549_create_letters_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('letters', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('address');
+            $table->string('city');
+            $table->dateTime('arrival_letter');
+            $table->string('present');
+            $table->text('content_letter');
+            $table->tinyInteger('rating');
+            $table->tinyInteger('delivered')->default(0);
             $table->timestamps();
         });
     }
