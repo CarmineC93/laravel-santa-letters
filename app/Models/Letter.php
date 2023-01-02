53d2652c12035e61c3403950bc1304125017b486
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-02 17:55:34 +0100

    create a private function to validate. Add store function to controller

--- app/Models/Letter.php
+++ app/Models/Letter.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Letter extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'lastname', 'address', 'city', 'arrival_letter', 'present', 'content_letter', 'rating', 'delivered'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Guest\PageController;
+use App\Http\Controllers\Admin\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
