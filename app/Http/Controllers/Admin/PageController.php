3b9722141de37d185feb298968059cc2e59bb56a
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-02 17:54:19 +0100

    change name to folder Guest->Admin

---
+++ app/Http/Controllers/Admin/PageController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Letter;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
+class PageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $letters = Letter::all();
+        return view('letters.index', compact('letters'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('letters.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $data = $this->validation($request->all());
+        $letter = new Letter();
+        $letter->fill($data);
+
+        $letter->save();
+
+        return redirect()->route('letters.show', $letter->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+
+    //qui una funzione che ci permatta di non riscrivere stesso codice e nel quale possiamo inserire array con messaggi di errore per ogni validazione non rispettata
+    private function validation($data)
+    {
+        // la class Validator da usare estende Facade
+        $validationResult  = Validator::make(
+            $data,
+            [
+                'name' => 'required|min:5|max:100',
+                'lastname' => 'required',
+                'address' => 'required',
+                'city' => 'required',
+                'arrival_letter' => 'required',
+                'present' => 'required',
+                'content_letter' => 'required',
+                'rating' => 'required',
+                'delivered' => 'required'
+
+            ],
+            [
+                'name.required' => 'E\' necessario inserire il proprio nome',
+                'name.min' => 'Il nome deve essere di almeno :min caratteri',
+                'name.max' => 'Il nome deve essere di al massimo :max caratteri',
+                'lastname.required' => 'Il cognome è necessario',
+                'address.required' => 'L\'indirizzo è necessario',
+                'city.required' => 'La città è necessaria',
+                'arrival_letter.required' => 'E\' necessario inserire una data di arrivo',
+                'present.required' => 'E\' necessario inserire il regalo',
+                'content_letter.required' => 'E\' necessario inserire il testo contenuto nella lettera',
+                'rating.required' => 'E\' necessario inserire il punteggio del bambino',
+                'delivered.required' => 'E\' necessario segnalare se il regalo è stato spedito'
+            ]
+        )->validate();
+        return $validationResult;
+    }
+}
